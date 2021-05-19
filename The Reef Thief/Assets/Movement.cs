242cb8cc144757121d71b462106499396af0fb66
omeedjamali <omeedj02@gmail.com>
2021-05-19 11:24:04 -0500

    Test pull
    this is to see if pull is working on other end

---
+++ The Reef Thief/Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Rigidbody2D rigidBody;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
