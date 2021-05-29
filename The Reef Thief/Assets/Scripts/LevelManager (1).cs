b06b9dc6830db9d23b4f3ba65af514fc61624195
Omeed Jamali <OJamali@student.naperville203.org>
2021-05-28 20:25:37 -0500

    Monster movement
    New Monster Sprite that also moves

---
+++ The Reef Thief/Assets/Scripts/LevelManager (1).cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void loadLevel(string level) 
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void quitlevel()
+    {
+        Application.Quit();
+    }
+
+    public void BrickDestroyed()
+    {
+            loadLevel("Win");
+    }
+
+}
---
+++ The Reef Thief/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    public static int movespeed = 2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       transform.Translate(Vector3.left * movespeed * Time.deltaTime);
+    }
+}
